6faf36c8ee788932624fd2f600cec1aae0613430
Gin-Chan <erdongerzong@qq.com>
2016-04-27 14:01:52 +0800

    Added files via upload

---
+++ gmane/gline.js
@@ -0,0 +1,5 @@
+gline = [ ['Year','umich.edu','cam.ac.uk','indiana.edu','uct.ac.za','gmail.com','berkeley.edu','unicon.net','stanford.edu','ucdavis.edu','mac.com'],
+['2005',57,7,12,14,10,12,6,4,11,12],
+['2006',1305,596,627,425,426,401,388,263,302,401],
+['2007',908,520,408,373,365,324,201,261,199,89]
+];
